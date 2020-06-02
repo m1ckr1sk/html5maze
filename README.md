adb40c2a2c7e7388009dbc455b92ad30148f003c
mike.butt <mike.butt@os.uk>
2020-06-02 23:11:32 +0100

    Added some agents

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
-# html5maze
-A maze generator in html 5
+# HTML 5 Maze
+
+A maze generator in html 5 with agents that travel through the maze.
+
+![alt text](https://github.com/m1ckr1sk/html5maze/blob/master/images/image1.png "Maze")
--- maze.js
+++ maze.js
@@ -7,35 +7,81 @@ class Box {
         this.bottom = false;
     }
 
-    set leftBlocked(leftBlockedValue)
-    {
+    set leftBlocked(leftBlockedValue) {
         this.left = leftBlockedValue;
     }
-    set rightBlocked(rightBlockedValue)
-    {
+    set rightBlocked(rightBlockedValue) {
         this.right = rightBlockedValue;
     }
-    set topBlocked(topBlockedValue)
-    {
+    set topBlocked(topBlockedValue) {
         this.top = topBlockedValue;
     }
-    set bottomBlocked(bottomBlockedValue)
-    {
+    set bottomBlocked(bottomBlockedValue) {
         this.bottom = bottomBlockedValue;
     }
 
-    get isFullyBlocked(){
+    get isFullyBlocked() {
         return this.left && this.right && this.top && this.bottom
     }
 };
-function draw_line(startx, starty, endx, endy) {
-    // Staring point (10,45)
-    ctx.moveTo(startx, starty);
-    // End point (180,47)
-    ctx.lineTo(endx, endy);
-    // Make the line visible
-    ctx.stroke();
+
+
+/*=============================================================================*/
+/* Variables
+/*=============================================================================*/
+var canvas = document.getElementById("MazeCanvas");
+var ctx = canvas.getContext('2d');
+var offscreenCanvas = document.createElement('canvas');
+offscreenCanvas.width = canvas.width;
+offscreenCanvas.height = canvas.height;
+var offCtx = offscreenCanvas.getContext('2d');
+
+var discoFloorCanvas = document.createElement('canvas');
+discoFloorCanvas.width = canvas.width;
+discoFloorCanvas.height = canvas.height;
+var discoCtx = discoFloorCanvas.getContext('2d');
+
+var grid_size = 20;
+var size = 20;
+var start_x = 10;
+var start_y = 45;
+var density = 0.4;
+var density_in_use = 1.0 - density;
+
+var currentAgentBoxes = [];
+var currentAgentColours = [];
+for (var i = 0; i <= grid_size; i++) {
+    currentAgentBoxes.push(i);
+    currentAgentColours.push(getRandomColor())
 }
+
+var moveTime = 100;
+var move_then = Date.now();
+
+var fps = 10;
+var fpsInterval = 1000 / fps;
+var then = Date.now();
+var startTime = then;
+var requestAnimationFrame = window.requestAnimationFrame ||
+    window.mozRequestAnimationFrame ||
+    window.webkitRequestAnimationFrame ||
+    window.msRequestAnimationFrame;
+
+
+createMaze();
+renderMaze();
+renderDiscoFloor()
+renderAgent();
+
+/*=============================================================================*/
+/* Utility Functions
+/*=============================================================================*/
+function draw_line(startx, starty, endx, endy) {
+    offCtx.fillStyle = '#000000'
+    offCtx.moveTo(startx, starty);
+    offCtx.lineTo(endx, endy);
+    offCtx.stroke();
+};
 
 function getRandomColor() {
     var letters = '0123456789ABCDEF';
@@ -44,19 +90,13 @@ function getRandomColor() {
         color += letters[Math.floor(Math.random() * 16)];
     }
     return color;
-}
-var canvas = document.getElementById('DemoCanvas');
-//Always check for properties and methods, to make sure your code doesn't break in other browsers.
-if (canvas.getContext) {
-    var ctx = canvas.getContext('2d');
-    // Reset the current path
-    ctx.beginPath();
-    grid_size = 50;
-    var size = 10;
-    start_x = 10;
-    start_y = 45;
-    density = 0.4;
-    density_in_use = 1.0 - density;
+};
+
+/*=============================================================================*/
+/* Create Maze
+/*=============================================================================*/
+function createMaze() {
+
     boxes = [];
     var x;
     current_x = start_x;
@@ -67,61 +107,173 @@ if (canvas.getContext) {
             boxes.push(new Box(current_box))
 
             console.log('Assessing box ' + current_box)
+
+            if(current_box < grid_size){
+                boxes[current_box].topBlocked = true;
+            }
+
+            if(current_box >= grid_size * (grid_size - 1)){
+                boxes[current_box].bottomBlocked = true;
+            }
+
+            if((current_box % grid_size) == 0){
+                boxes[current_box].leftBlocked = true;
+            }
+
+            if(((current_box + 1) % grid_size) == 0){
+                boxes[current_box].rightBlocked = true;
+            }
+
             if (Math.random() > density_in_use) {
-                top_box =  current_box;
-                bottom_box =  (x + (grid_size * (y-1)));
+                top_box = current_box;
+                bottom_box = (x + (grid_size * (y - 1)));
                 console.log('Blocking top box:' + top_box);
-                if(bottom_box > -1)
-                {
+                if (bottom_box > -1) {
                     console.log('Blocking bottom box:' + bottom_box);
                     boxes[bottom_box].bottomBlocked = true;
                 }
-                boxes[top_box].topBlocked = true;                     
-                draw_line(current_x, current_y, current_x + size, current_y);
+                boxes[top_box].topBlocked = true;
             }
 
             if (Math.random() > density_in_use) {
                 left_box = current_box;
-                
+
                 console.log('Blocking left box ' + left_box);
-                draw_line(current_x, current_y, current_x, current_y + size);
+
                 boxes[left_box].leftBlocked = true;
-                
-                if(x > 0){
+
+                if (x > 0) {
                     right_box = current_box - 1;
                     boxes[right_box].rightBlocked = true;
                     console.log('Blocking right box ' + right_box);
                 }
             }
-
             current_x = current_x + size;
-
         }
         current_y = current_y + size;
         current_x = start_x;
     }
-}
-ctx.closePath();
-current_x = start_x;
-current_y = start_y;
 
-for (y = 0; y < grid_size; y++) {
-    for (x = 0; x < grid_size; x++) {
-        current_box = x + ( y * grid_size);
-        if(boxes[current_box].isFullyBlocked){
-            ctx.globalAlpha = 1.0;
-            ctx.fillStyle = '#000000'
-            console.log('Box is filled ' + current_box)
-        }
-        else{
-            ctx.globalAlpha = 0.2;
-            ctx.fillStyle = getRandomColor();
-        }
-        
-        ctx.fillRect(current_x, current_y, size, size);
-        current_x = current_x + size;
-    }
+    return boxes;
+};
+
+
+function renderMaze() {
+
     current_x = start_x;
-    current_y = current_y + size;
-}
-ctx.globalAlpha = 1.0;
+    current_y = start_y;
+
+    ctx.beginPath();
+
+    for (y = 0; y < grid_size; y++) {
+        for (x = 0; x < grid_size; x++) {
+            current_box = x + (y * grid_size);
+            if (!boxes[current_box].isFullyBlocked) {
+
+                if (boxes[current_box].left) {
+                    draw_line(current_x, current_y, current_x, current_y + size);
+                }
+                if (boxes[current_box].right) {
+                    draw_line(current_x + size, current_y, current_x + size, current_y + size);
+                }
+                if (boxes[current_box].top) {
+                    draw_line(current_x, current_y, current_x + size, current_y);
+                }
+                if (boxes[current_box].bottom) {
+                    draw_line(current_x, current_y + size, current_x + size, current_y + size);
+                }
+            }
+
+            current_x = current_x + size;
+        }
+        current_x = start_x;
+        current_y = current_y + size;
+    }
+    ctx.closePath();
+};
+
+function renderDiscoFloor() {
+
+    current_x = start_x;
+    current_y = start_y;
+
+    for (y = 0; y < grid_size; y++) {
+        for (x = 0; x < grid_size; x++) {
+            current_box = x + (y * grid_size);
+            if (boxes[current_box].isFullyBlocked) {
+                offCtx.fillStyle = '#000000'
+                offCtx.fillRect(current_x, current_y, size, size);
+            }
+            else {
+                discoCtx.fillStyle = getRandomColor();
+                discoCtx.fillRect(current_x, current_y, size, size);
+            }
+            current_x = current_x + size;
+        }
+        current_x = start_x;
+        current_y = current_y + size;
+    }
+};
+
+function renderAgent() {
+    // request another frame
+    requestAnimationFrame(renderAgent);
+ 
+
+    // calc elapsed time since last loop
+    now = Date.now();
+    elapsed = now - then;
+    elapsed_move = now - move_then;
+
+    then = now - (elapsed % fpsInterval);
+    move_then = now - (elapsed_move % moveTime);
+
+    // if enough time has elapsed, draw the next frame
+    if (elapsed > fpsInterval) {
+
+        ctx.clearRect(0, 0, canvas.width, canvas.height);
+        ctx.globalAlpha = 1.0;
+        ctx.drawImage(offscreenCanvas, 0, 0);
+        ctx.globalAlpha = 0.3;
+        ctx.drawImage(discoFloorCanvas, 0, 0);
+
+        for(agentIndex = 0; agentIndex < currentAgentBoxes.length;agentIndex++){
+            agent_x = start_x + ((currentAgentBoxes[agentIndex] % grid_size) * size);
+            agent_y = start_y + (Math.floor(currentAgentBoxes[agentIndex] / grid_size) * size);
+            ctx.fillStyle = currentAgentColours[agentIndex];
+            ctx.globalAlpha = ((agent_y - start_y) / ((start_y + (grid_size * size)) - start_y));
+            ctx.fillRect(agent_x, agent_y, size, size);
+        }
+    }
+
+    if(elapsed_move > moveTime){
+        for(agentIndex = 0; agentIndex < currentAgentBoxes.length;agentIndex++){
+
+            if(!boxes[currentAgentBoxes[agentIndex]].bottom){
+                currentAgentBoxes[agentIndex] = currentAgentBoxes[agentIndex] + grid_size;
+            }
+            else{
+                if(Math.floor(Math.random() > 0.5)){
+                    // left then right
+                    if(!boxes[currentAgentBoxes[agentIndex]].left){
+                        currentAgentBoxes[agentIndex] = currentAgentBoxes[agentIndex] - 1;
+                    }
+                    else if(!boxes[currentAgentBoxes[agentIndex]].right){
+                        currentAgentBoxes[agentIndex] = currentAgentBoxes[agentIndex] + 1;
+                    }
+                }
+                else{
+                    //right then left
+                    if(!boxes[currentAgentBoxes[agentIndex]].right){
+                        currentAgentBoxes[agentIndex] = currentAgentBoxes[agentIndex] + 1;
+                    }
+                    else if(!boxes[currentAgentBoxes[agentIndex]].left){
+                        currentAgentBoxes[agentIndex] = currentAgentBoxes[agentIndex] - 1;
+                    }
+                }
+            }
+        }
+    }
+};
+
+
